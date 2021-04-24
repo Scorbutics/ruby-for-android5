775814af85c0bc5524a47a50e6c2060d5a9e2a03
Brice BULGARELLI <brice.bulgarelli@gmail.com>
2021-04-24 17:01:47 +0200

    fix : cleanup + explicit libc++_shared

--- arm64-v8a-android-toolchain.cmake
+++ arm64-v8a-android-toolchain.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME "Android")
 set(CMAKE_SYSTEM_VERSION 26) # API level
 set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 set(CMAKE_ANDROID_NDK /usr/local/android/android-ndk-r21e)
-#set(CMAKE_ANDROID_STL_TYPE gnustl_static)
+set(CMAKE_ANDROID_STL_TYPE c++_shared)
 
 # Used for configure params --host & --build
 set(HOST "aarch64-linux-android")
